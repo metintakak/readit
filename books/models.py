087c18cce1d722c8d087889b6fb87f19def467ea
metintakak@gmail.com <metintakak@example.com>
2016-07-31 12:38:53 +0300

    added new model author and craeted many to many relationship from book model

--- books/models.py
+++ books/models.py
@@ -3,10 +3,17 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length = 150)
-    author = models.CharField(max_length = 70, help_text = "you can use a pen name, original name is optional")
+    authors = models.ManyToManyField("Author", related_name = "books")
     review = models.TextField(blank = True, null= True)
     date_reviewed = models.DateTimeField(blank = True, null = True)
     is_favoriote = models.BooleanField(default =  False, verbose_name = "Favorite?")
 
     def __str__(self):
         return self.title
+
+
+class Author(models.Model):
+    name = models.CharField(max_length = 70, help_text = "you can use a pen name, original name is optional", unique = True)
+
+    def __str__(self):
+        return self.name
