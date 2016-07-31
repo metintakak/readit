7dfd0d767ff7ace796147cf7334b2f62cab9e45c
metintakak@gmail.com <metintakak@example.com>
2016-07-31 16:22:44 +0300

    enhanced the view. query the books with joins

--- books/views.py
+++ books/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
 
 # Create your views here.
 def list_books(request):
-    return render(request,"list.html")
+    """
+    list the books that has reviews
+    """
+
+    books = Book.objects.exclude(date_reviewed__isnull=True).prefetch_related('authors')
+    context = {
+        "books" : books,
+    }
+
+    return render(request,"list.html",context)
     #you can access the login user information
     #return HttpResponse(request.user.username)
