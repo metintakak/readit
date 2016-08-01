47c8e8f206098920d365d776dfdf58a95357963f
metintakak@gmail.com <metintakak@example.com>
2016-08-01 23:25:05 +0300

    add list of books to review , book review page,first form

---
+++ books/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+class ReviewForm(forms.Form):
+    """
+        form for reviewing a book
+    """
+
+    is_favoriote = forms.BooleanField(
+        label = "Favorite?",
+        help_text = "if it is in yor top 100 books of all time",
+        required = False
+    )
+
+    review = forms.CharField(
+        widget = forms.Textarea,
+        min_length = 50,
+        error_messages =  {
+            'required' : 'Please enter your review',
+            'min_length' : 'please enter at leats 50 characters. (you have written out %(show_value)s)'
+        }
+    )
--- books/views.py
+++ books/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Book, Author
 from django.views.generic import DetailView, View
 from django.db.models import Count
+from .forms import ReviewForm
 
 # Create your views here.
 def list_books(request):
@@ -44,4 +45,33 @@ class BookDetail(DetailView):
 
 class AuthorDetail(DetailView):
     model = Author
-    template_name = "author.html"    
+    template_name = "author.html"
+
+# Paste into Views.py - don't forget to import get_object_or_404!
+def review_books(request):
+	"""
+	List all of the books that we want to review.
+	"""
+	books = Book.objects.filter(date_reviewed__isnull=True).prefetch_related('authors')
+
+	context = {
+		'books': books,
+	}
+
+	return render(request, "list-to-review.html", context)
+
+
+def review_book(request, pk):
+    """
+    Review an individual book
+    """
+    book = get_object_or_404(Book, pk=pk)
+
+    form = ReviewForm
+
+    context = {
+    	'book': book,
+        'form' : form,
+    }
+
+    return render(request, "review-book.html", context)
