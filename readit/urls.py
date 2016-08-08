6f56f2b0e709be6f0a0a97123068152430c8b58d
metintakak@gmail.com <metintakak@example.com>
2016-08-09 00:14:42 +0300

    login and logout links and form

--- readit/urls.py
+++ readit/urls.py
@@ -16,15 +16,18 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import views as auth_views
 
 from books.views import (list_books, AuthorList, AuthorDetail, BookDetail,
-                            ReviewList, review_book,CreateAuthor)
+                         ReviewList, review_book, CreateAuthor)
 
 urlpatterns = [
+    url(r'^logout/$', auth_views.logout, {'next_page': 'books'}, name="logout"),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name="login"),
     url(r'^admin/', admin.site.urls),
-    url(r'^$', list_books, name = "books"),
-    url(r'^authors/$', AuthorList.as_view(), name = "authors"),
-    url(r'^books/(?P<pk>[-\w]+)/$', BookDetail.as_view(), name = "book-detail"),
+    url(r'^$', list_books, name="books"),
+    url(r'^authors/$', AuthorList.as_view(), name="authors"),
+    url(r'^books/(?P<pk>[-\w]+)/$', BookDetail.as_view(), name="book-detail"),
     url(r'^authors/add/$', login_required(CreateAuthor.as_view()), name='add-author'),
     url(r'^authors/(?P<pk>[-\w]+)/$', AuthorDetail.as_view(), name='author-detail'),
     url(r'^review/$', login_required(ReviewList.as_view()), name='review-books'),
